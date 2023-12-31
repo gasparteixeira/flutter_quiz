097b74c69711d2271679a080774cd73e32b553af
gasparteixeira <gaspar.teixeira@gmail.com>
2023-12-31 16:58:23 -0500

    adding quiz class to switch between screens

---
+++ lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/questions_screens.dart';
+import 'package:quiz/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 29, 8, 66),
+                Color.fromARGB(255, 108, 62, 188),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
